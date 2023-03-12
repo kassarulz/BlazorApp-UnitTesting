fcbe88c7145f67ac4b750531abdedbb74c247316
Kasun Perera <Kasun.P@CreativeSoftware.com>
2023-03-12 09:53:35 +0530

    Unit tests added

---
+++ UnitsTests/MyBlazorAppTests.cs
@@ -0,0 +1,36 @@
+﻿using BlazorApp.Pages;
+using Bunit;
+using Xunit;
+
+namespace UnitTests
+{
+    public class MyBlazorAppTests : TestContext
+    {
+        [Fact]
+        public void Counter_Increments_Count_When_Button_Is_Clicked()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Act
+            cut.Find("button").Click();
+
+            // Assert
+            var actualCount = int.Parse(cut.Find("#current-count").InnerHtml);
+            Assert.Equal(1, actualCount);
+        }
+
+        /*[Theory]
+        [InlineData("John")]
+        [InlineData("Jane")]
+        public void Greeting_Displays_Correct_Message(string name)
+        {
+            // Arrange
+            var cut = RenderComponent<Greetings>(
+                ("Name", name));
+
+            // Assert
+            cut.MarkupContains($"Hello, {name}!");
+        }*/
+    }
+}
